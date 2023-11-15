3340b8bffa8d90e13bc4bf27a7e34e6aba35c992
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-11-15 13:29:40 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Giordan Zeina
+ * Created on: Nov 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+let loopCounter1 = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+
+// When "A" is pressed, the pixels move 
+input.onButtonPressed(Button.A, function () {
+  // setup
+  basic.clearScreen()
+  loopCounter = 0
+  sprite = game.createSprite(0, 0)
+
+  while (loopCounter1 <= 3) {
+    sprite.move(1)
+    loopCounter = loopCounter + 1
+    basic.pause(500)
+
+    while (loopCounter >= 4) {
+      sprite.turn(Direction.Right, 90)
+      loopCounter1 = loopCounter1 + 1
+      basic.pause(500)
+      loopCounter = 0
+    }
+    
+  }
+
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
+})
+
